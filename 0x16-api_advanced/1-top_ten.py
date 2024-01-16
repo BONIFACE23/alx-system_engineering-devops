14b51d0146042bbb4e6b050b7a7fd34eb0cd74b4
BONIFACE23 <irunguboniface413@gmail.com>
2024-01-16 14:14:42 -0800

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Model to queries the Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """ Queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit. """
+    try:
+        response = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+                .format(subreddit), allow_redirects=False)
+        if response.status_code == '404':
+            print(None)
+            return
+        data = response.json()
+        titles = []
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    except Exception:
+        print(None)
